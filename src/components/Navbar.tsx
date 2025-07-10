f5d5b8518c894c7c66f7e22fe69861691c37d1c6
Atharva-1308 <agrawalatharva.2008@gmail.com>
2025-07-11 01:17:15 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -168,16 +168,3 @@ export const Navbar: React.FC = () => {
     </nav>
   );
 };
-            <button
-              onClick={toggleTheme}
-              className="p-2 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
-              aria-label="Toggle theme"
-            >
-              {isDarkMode ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
